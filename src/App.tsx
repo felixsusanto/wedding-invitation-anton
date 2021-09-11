321d031043540274ff9ef0b2657bf59d62dabad8
felix_susanto <felix.susanto2000@gmail.com>
2021-09-11 23:36:49 +0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import cover from "./img/Don-Den-01-cover.jpg";
+import inv from "./img/Don-Den-02-inv.jpg";
+import bg from "./img/bg.jpg";
+
+const AppContainer = styled.div`
+  background: url("${bg}");
+  background-size: cover;
+`;
+
+const FirstContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const SecondContainer = styled.div`
+  height: 100vh;
+`;
+
+const InvitationCover = styled.div`
+  position: relative;
+  z-index: 1;
+  height: 90%;
+  box-shadow: 0 5vw 10vw rgb(0 0 0 / 30%);
+  img {
+    height: 100%;
+    width: auto;
+  }
+  &:before {
+    /* @media (orientation: portrait) {
+      content: "portrait";
+    }
+    @media (orientation: landscape) {
+      content: "landscape";
+    } */
+  }
+`;
+
+const Invitation = styled.img`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  height: 88vh;
+  transform: translateX(-50%) translateY(-50%);
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Invitation src={inv} />
+      <FirstContainer>
+        <InvitationCover>
+          <img src={cover} alt="cover invitation" />
+        </InvitationCover>
+      </FirstContainer>
+      <SecondContainer />
+    </AppContainer>
   );
 }
 
